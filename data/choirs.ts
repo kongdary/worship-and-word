7bc220abae36a75c91506726151c7557a3790392
kongdary <kongdary@users.noreply.github.com>
2026-08-12 04:54:21 +0900

    Add official choir video links

--- data/choirs.ts
+++ data/choirs.ts
@@ -1,8 +1,8 @@
 import { Choir } from "@/types/choir";
 export const choirs: Choir[] = [
  {id:"zion",name:"시온찬양대",description:"찬양으로 하나님께 영광을 올려드립니다.",image:"✦",youtubeKeyword:"시온찬양대",sortOrder:1},
- {id:"choir2",name:"찬양대2",description:"감사와 기쁨으로 드리는 찬양입니다.",image:"♪",youtubeKeyword:"찬양대2",sortOrder:2},
- {id:"choir3",name:"찬양대3",description:"마음을 모아 아름다운 화음을 전합니다.",image:"◌",youtubeKeyword:"찬양대3",sortOrder:3},
- {id:"choir4",name:"찬양대4",description:"은혜의 고백을 노래합니다.",image:"♬",youtubeKeyword:"찬양대4",sortOrder:4},
- {id:"choir5",name:"찬양대5",description:"다음 세대와 함께하는 찬양입니다.",image:"✧",youtubeKeyword:"찬양대5",sortOrder:5},
+ {id:"hallelujah",name:"할렐루야찬양대",description:"감사와 기쁨으로 드리는 찬양입니다.",image:"♪",youtubeKeyword:"할렐루야 찬양대",sortOrder:2},
+ {id:"hosanna",name:"호산나찬양대",description:"마음을 모아 아름다운 화음을 전합니다.",image:"◌",youtubeKeyword:"호산나 찬양대",sortOrder:3},
+ {id:"bethlehem",name:"베들레헴찬양대",description:"은혜의 고백을 노래합니다.",image:"♬",youtubeKeyword:"베들레헴 찬양대",sortOrder:4},
+ {id:"gabriel",name:"가브리엘찬양대",description:"다음 세대와 함께하는 찬양입니다.",image:"✧",youtubeKeyword:"가브리엘 찬양대",sortOrder:5},
 ];
